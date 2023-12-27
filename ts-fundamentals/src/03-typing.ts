f86f646a4924dd6dcb410fa388b82e71704203cf
Facundo <fblanco@oneplay.net>
2023-12-27 11:24:49 -0300

    Inferred types

---
+++ ts-fundamentals/src/03-typing.ts
@@ -0,0 +1,11 @@
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 100;
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 100;
+  const myProductName2 = 'Product 2';
+})();
